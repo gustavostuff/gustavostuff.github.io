9d0f463d4119772025aa253514a310c6a0e37a2c
Gus <tavuntusoftware@gmail.com>
2019-08-30 22:30:02 -0700

    keanu cursor not in mobile

--- js/site.js
+++ js/site.js
@@ -102,4 +102,13 @@ const toggleKeanu = () => {
 
   window.addEventListener("mousemove", moveKeanu, false);
 
+  if (navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i) {
+    document.querySelector('#cursor-btn').style.display = 'none';
+  };
 })()
